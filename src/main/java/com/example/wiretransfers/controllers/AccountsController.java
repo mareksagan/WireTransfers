d8fc3378bea83f2b73ac5d23e53fbb9a3ad554a9
Marek Sagan <marek.sagan@protonmail.com>
2019-07-23 12:19:17 +0200

    Added H2 database support

--- src/main/java/com/example/wiretransfers/controllers/AccountsController.java
+++ src/main/java/com/example/wiretransfers/controllers/AccountsController.java
@@ -1,5 +1,11 @@
 package com.example.wiretransfers.controllers;
 
+/*  The API will expose an endpoint which accepts the user information (customerID, initialCredit).
+    Once the endpoint is called, a new account will be opened connected to the user whose ID is customerID.
+    If initialCredit is not 0, a transaction will be sent to the new account.
+    Another Endpoint will output the user information showing Name, Surname, balance, and transactions of the accounts
+*/
+
 public class AccountsController {
 
 }
